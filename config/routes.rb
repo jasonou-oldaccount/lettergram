6f19e2c6ea61cf551904eb96376b94258fe2c256
jasonou <jasonou95@gmail.com>
2014-10-04 17:01:35 +0000

    gallery and design added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       resources :users
       resources :sessions
       resources :letter
+      resources :gallery
+      resources :design
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
